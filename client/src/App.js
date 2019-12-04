42e1b4219450e8a34c810394c7be3d3a2aea3859
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-12-04 16:22:12 -0500

    added some reviews and fixed some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import Header from './components/Header'
 import ChipsList from './components/ChipsList'
 import SingleChip from './components/SingleChip';
 import CreateReview from './components/CreateReview';
-import EditReview from './components/EditReview'
+import EditReview from './components/EditReview';
+// import ChipsHeroPic from './components/ChipsHeroPic';
 
 import { Route, Link, withRouter } from 'react-router-dom';
 
@@ -56,32 +57,6 @@ class App extends Component {
     this.setState({
       reviews
     })
-  }
-
-  newReview = async (e) => {
-    e.preventDefault();
-    const review = await createReview(this.props.chipId, this.props.reviewForm);
-    this.setState(prevState => ({
-      reviews: [...prevState.reviews, review],
-      reviewForm: {
-        cost: 0,
-        taste: 0,
-        guilt: 0,
-        review: ""
-      }
-    }))
-  }
-
-  editReview = async () => {
-    const { reviewForm } = this.state
-    await updateReview(reviewForm.id, reviewForm)
-    this.setState(prevState => (
-      {
-        reviews: prevState.reviews.map(review => {
-          return review.id === reviewForm.id ? reviewForm : review
-        })
-      }
-    ))
   }
 
   mountEditForm = async (id) => {
@@ -147,10 +122,25 @@ class App extends Component {
           handleLogout={this.handleLogout}
           currentUser={this.state.currentUser}
         />
+        {/* <Route exact path="/" render={() => (
+          <ChipsHeroPic />
+        )} /> */}
+
         <Route exact path="/" render={() => (
-          <ChipsList
-            chips={this.state.chips}
-          />)}
+          <React.Fragment>
+            <div className="cover-img-div">
+              <img id="cover-img" src="https://previews.123rf.com/images/niloo138/niloo1381503/niloo138150300006/38140413-potato-chips-and-snacks-on-shelves-in-a-supermarket.jpg" />
+              <a href="chips-list-div">
+                <h2 id="cover-img-h2">Find Chips to Quip</h2>
+                <i id="arrow-down" class="material-icons animated bounce">play_for_work</i>
+              </a>
+
+            </div>
+            <ChipsList
+              chips={this.state.chips}
+            />
+          </React.Fragment>
+        )}
         />
 
         <Route exact path="/chips/:chipId" render={(props) =>
@@ -160,6 +150,7 @@ class App extends Component {
             mountEditForm={this.mountEditForm}
           />}
         />
+
         <Route exact path="/login" render={() => (
           <Login
             handleLogin={this.handleLogin}
@@ -178,44 +169,16 @@ class App extends Component {
         <Route
           exact path="/chips/:chipId/review" render={(props) => (
             <CreateReview
-              // handleFormChange={this.handleFormChange}
-              // reviewForm={this.state.reviewForm}
-              // newReview={this.newReview}
               chipId={props.match.params.chipId}
             />
-
           )} />
-        
+
         <Route
           exact path="/chips/:chipId/review/edit/:reviewId" render={(props) => (
             <EditReview
-              // handleFormChange={this.handleFormChange}
-              // reviewForm={this.state.reviewForm}
-              // mountEditForm={this.mountEditForm}
-              // newReview={this.newReview}
               chipId={props.match.params.chipId}
             />
-
           )} />
-        
-
-        {/* <Route
-          path="/reviews/:id"
-          render={(props) => {
-            const { id } = props.match.params;
-            const review = this.state.reviews.find(ele => ele.id === parseInt(id));
-            return <ReviewPage
-              id={id}
-              review={review}
-              handleFormChange={this.handleFormChange}
-              mountEditForm={this.mountEditForm}
-              editReview={this.editReview}
-              commentForm={this.state.commentForm}
-            // deleteComment={this.deleteComment}
-            />
-          }}
-        /> */}
-
       </div>
     );
   }
--- client/src/components/ChipsList.jsx
+++ client/src/components/ChipsList.jsx
@@ -5,14 +5,14 @@ export default function ChipsList(props) {
 
   return (
     <div id="chips-div">
-      <div className="cover-img-div">
+      {/* <div className="cover-img-div">
         <img id="cover-img" src="https://previews.123rf.com/images/niloo138/niloo1381503/niloo138150300006/38140413-potato-chips-and-snacks-on-shelves-in-a-supermarket.jpg" />
         <a href="chips-list-div">
           <h2 id="cover-img-h2">Find Chips to Quip</h2>
           <i id="arrow-down" class="material-icons animated bounce">play_for_work</i>
         </a>
 
-      </div>
+      </div> */}
       <div className="chips-list-div">
         {props.chips.map(chip => (
           <React.Fragment key={chip.id}>
